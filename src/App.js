dd650e2197819cbec09db2b90ced2bc325d10996
Taneros <fricentaurus@GMAIL.COM>
2022-07-14 19:47:59 +0300

    feat: featured section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Route, Routes } from 'react-router-dom'
+import { Featured } from './components/Featured'
 import { Hero } from './components/Hero'
 import { Navbar } from './components/Navbar'
 
@@ -8,7 +9,15 @@ function App() {
       <Navbar />
       <Routes>
         {/* //TODO: create main container to put all inside */}
-        <Route path="/" element={<Hero />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Featured />
+            </>
+          }
+        />
       </Routes>
     </div>
   )
