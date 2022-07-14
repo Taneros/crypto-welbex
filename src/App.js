f3aab80197057cd5364d36bb4213fe68d40c898a
Taneros <fricentaurus@GMAIL.COM>
2022-07-14 16:22:34 +0300

    feat: navbar and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<div>hello</div>} />
       </Routes>
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import './style.css'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleShowMenu = () => {
+    setShowMenu((prev) => !prev)
+  }
+
+  return (
+    <div className="header">
+      <div className="container">
+        <h1 style={{ position: 'absolute', top: '-500px', left: '-500px' }}>
+          CryptoWelbeX
+        </h1>
+        <h2>
+          Crypto<span className="primary">WelbeX</span>
+        </h2>
+        <ul className={showMenu ? 'nav-menu active' : 'nav-menu'}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">List</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+        <div className="btn-group">
+          <button className="btn">Button</button>
+        </div>
+        <div className="humburger" onClick={handleShowMenu}>
+          {showMenu ? (
+            <FaTimes size={25} style={{ color: '#333' }} />
+          ) : (
+            <FaBars size={25} style={{ color: '#333' }} />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
